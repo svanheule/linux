dff2f102e5f1580a33b114c2210fd5d7853f289c
Sander Vanheule <sander@svanheule.net>
2022-08-12 19:03:54 +0200

    mfd: rtl-switchcore: remove sys-led parts

--- drivers/mfd/realtek-switchcore.c
+++ drivers/mfd/realtek-switchcore.c
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include <linux/bitfield.h>
-#include <linux/leds.h>
 #include <linux/mfd/core.h>
 #include <linux/mfd/syscon.h>
 #include <linux/module.h>
@@ -14,7 +13,6 @@
 struct realtek_switchcore_ctrl;
 
 struct realtek_switchcore_data {
-	struct reg_field sys_led_field;
 	const struct mfd_cell *mfd_devices;
 	unsigned int mfd_device_count;
 	void (*probe_model_name)(const struct realtek_switchcore_ctrl *ctrl);
@@ -24,9 +22,6 @@ struct realtek_switchcore_ctrl {
 	struct device *dev;
 	struct regmap *map;
 	const struct realtek_switchcore_data *data;
-	struct led_classdev sys_led;
-	struct regmap_field *sys_led_field;
-	bool active_low;
 };
 
 #define MODEL_NAME_CHAR_XLATE(val)		((val) ? 'A' + (val) - 1 : '\0')
@@ -96,92 +91,9 @@ static void rtl8390_probe_model_name(const struct realtek_switchcore_ctrl *ctrl)
  *
  * The switch SoC supports one hardware managed direct LED output
  * to manage a system LED, with two supported blinking rates.
+ *
+ * TODO Move back here from split driver
  */
-enum {
-	SWITCHCORE_SYS_LED_OFF = 0,
-	SWITCHCORE_SYS_LED_BLINK_64MS,
-	SWITCHCORE_SYS_LED_BLINK_1024MS,
-	SWITCHCORE_SYS_LED_ON
-};
-
-static void switchcore_sys_led_set_rate(const struct realtek_switchcore_ctrl *ctrl,
-	unsigned int mode)
-{
-	regmap_field_write(ctrl->sys_led_field, mode);
-}
-
-static void switchcore_sys_led_brightness_set(struct led_classdev *led_cdev,
-	enum led_brightness brightness)
-{
-	struct realtek_switchcore_ctrl *ctrl =
-		container_of(led_cdev, struct realtek_switchcore_ctrl, sys_led);
-
-	if ((!ctrl->active_low && brightness == LED_OFF) ||
-		(ctrl->active_low && brightness != LED_OFF))
-		switchcore_sys_led_set_rate(ctrl, SWITCHCORE_SYS_LED_OFF);
-	else
-		switchcore_sys_led_set_rate(ctrl, SWITCHCORE_SYS_LED_ON);
-}
-
-static enum led_brightness switchcore_sys_led_brightness_get(
-	struct led_classdev *led_cdev)
-{
-	struct realtek_switchcore_ctrl *ctrl =
-		container_of(led_cdev, struct realtek_switchcore_ctrl, sys_led);
-	u32 val;
-
-	regmap_field_read(ctrl->sys_led_field, &val);
-
-	if ((!ctrl->active_low && val == SWITCHCORE_SYS_LED_OFF) ||
-		(ctrl->active_low && val == SWITCHCORE_SYS_LED_ON))
-		return LED_OFF;
-	else
-		return LED_ON;
-}
-
-static int switchcore_sys_led_blink_set(struct led_classdev *led_cdev,
-	unsigned long *delay_on, unsigned long *delay_off)
-{
-	struct realtek_switchcore_ctrl *ctrl =
-		container_of(led_cdev, struct realtek_switchcore_ctrl, sys_led);
-	u32 blink_interval = *delay_on + *delay_off;
-
-	/* Split range at geometric mean of 64 and 1024 */
-	if (blink_interval == 0 || blink_interval > 2 * 256) {
-		*delay_on = 1024;
-		*delay_off = 1024;
-		switchcore_sys_led_set_rate(ctrl, SWITCHCORE_SYS_LED_BLINK_1024MS);
-	} else {
-		*delay_on = 64;
-		*delay_off = 64;
-		switchcore_sys_led_set_rate(ctrl, SWITCHCORE_SYS_LED_BLINK_64MS);
-	}
-
-	return 0;
-}
-
-static int switchcore_sys_led_probe(struct realtek_switchcore_ctrl *ctrl,
-	struct device_node *np)
-{
-	struct led_classdev *sys_led = &ctrl->sys_led;
-	struct led_init_data init_data = {};
-
-	init_data.fwnode = of_fwnode_handle(np);
-
-	ctrl->sys_led_field =
-		devm_regmap_field_alloc(ctrl->dev, ctrl->map, ctrl->data->sys_led_field);
-	if (IS_ERR(ctrl->sys_led_field))
-		return PTR_ERR(ctrl->sys_led_field);
-
-	ctrl->active_low = of_property_read_bool(np, "active-low");
-
-	sys_led->max_brightness = 1;
-	sys_led->brightness_set = switchcore_sys_led_brightness_set;
-	sys_led->brightness_get = switchcore_sys_led_brightness_get;
-	sys_led->blink_set = switchcore_sys_led_blink_set;
-
-	return devm_led_classdev_register_ext(ctrl->dev, sys_led, &init_data);
-}
 
 static const struct mfd_cell rtl8380_mfd_devices[] = {
 	MFD_CELL_OF("realtek-switchcore-sys-led", NULL, NULL, 0, 0, "realtek,maple-sys-led"),
@@ -194,7 +106,6 @@ static const struct mfd_cell rtl8380_mfd_devices[] = {
 };
 
 static const struct realtek_switchcore_data rtl8380_switchcore_data = {
-	.sys_led_field = REG_FIELD(0xa000, 16, 17),
 	.mfd_devices = rtl8380_mfd_devices,
 	.mfd_device_count = ARRAY_SIZE(rtl8380_mfd_devices),
 	.probe_model_name = rtl8380_probe_model_name,
@@ -211,7 +122,6 @@ static const struct mfd_cell rtl8390_mfd_devices[] = {
 };
 
 static const struct realtek_switchcore_data rtl8390_switchcore_data = {
-	.sys_led_field = REG_FIELD(0x00e4, 15, 16),
 	.mfd_devices = rtl8390_mfd_devices,
 	.mfd_device_count = ARRAY_SIZE(rtl8390_mfd_devices),
 	.probe_model_name = rtl8390_probe_model_name,
@@ -234,10 +144,8 @@ static int realtek_switchcore_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
 	struct device_node *np = dev->of_node;
-	struct device_node *np_sys_led;
 	const struct of_device_id *match;
 	struct realtek_switchcore_ctrl *ctrl;
-	int err;
 
 	ctrl = devm_kzalloc(dev, sizeof(*ctrl), GFP_KERNEL);
 	if (!ctrl)
